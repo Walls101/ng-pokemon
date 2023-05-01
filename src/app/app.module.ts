63d267c5d82b8b36b741fd8ae743326169bb5b61
Alex Paredes <alexparedes83@gmail.com>
2023-05-01 08:29:52 -0600

    alex change sorry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/cards/card/card.component';
-import { Page4Component } from './page4/page4.component';
+import { Page4Component } from './components/pages/page4/page4.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppRoutingModule,
     Page4Component,
     CardComponent,
   ],
@@ -17,6 +19,7 @@ import { Page4Component } from './page4/page4.component';
     AppRoutingModule,
     Page4Component,
     CardComponent,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
