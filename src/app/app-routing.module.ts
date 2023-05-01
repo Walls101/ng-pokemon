fbf6d61f97fe598d0c0e0b540d0a89241205bb29
Alex Paredes <alexparedes83@gmail.com>
2023-05-01 11:39:57 -0600

    added routing to page4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './components/cards/card/card.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { Page4Component } from './components/pages/page4/page4.component';
 
 const routes: Routes = [
   { path: '', component: LoginPageComponent },
-  { path: 'show-cards', component: CardComponent }
+  { path: 'show-cards', component: CardComponent },
+  {path: 'page4', component: Page4Component}
 
 ];
 
