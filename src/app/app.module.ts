fc21892d467ef80afffb7c2e671ac278cdb64e86
Alex Paredes <alexparedes83@gmail.com>
2023-05-01 08:05:15 -0600

    page 4 work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page4Component } from './page4/page4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page4Component
   ],
   imports: [
     BrowserModule,
