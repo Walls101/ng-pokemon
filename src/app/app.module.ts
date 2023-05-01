51e4ce1d78b283d7c7b798bfe0f7ae6d9d5337f4
Jake <jdavies212@gmail.com>
2023-05-01 07:55:51 -0600

    mine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatCardModule} from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatCardModule } from '@angular/material/card'
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +13,8 @@ import { MatCardModule } from '@angular/material/card'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
